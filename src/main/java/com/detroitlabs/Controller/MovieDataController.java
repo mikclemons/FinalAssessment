6092d4c533920a27e0286a3a2a9092ac6c8880ce
Mikayla Clemons <mikaylaclemons@Mikaylas-MacBook-Pro.local>
2019-04-04 18:44:40 -0400

    Create html page to display all of the character names and make those names links.

--- src/main/java/com/detroitlabs/Controller/MovieDataController.java
+++ src/main/java/com/detroitlabs/Controller/MovieDataController.java
@@ -1,12 +1,17 @@
 package com.detroitlabs.Controller;
 
 import com.detroitlabs.Model.CharacterInfo;
+import com.detroitlabs.Model.CharacterUrl;
 import com.detroitlabs.Model.MovieData;
 import com.detroitlabs.Services.MovieDataService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class MovieDataController {
@@ -14,25 +19,34 @@ public class MovieDataController {
     @Autowired
     private MovieDataService movieService;
 
-    @ResponseBody
+
     @RequestMapping("/")
-    public String displayMovieData(){
+    public String displayMovieData(ModelMap modelMap, ModelMap modelMap1) {
         MovieData movieData = movieService.fetchMovieData();
-        return movieData.getTitle() + movieData.getEpisodeId() +
-                movieData.getOpening_Crawl() +
-                movieData.getDirector() +
-                movieData.getProducer() +
-                movieData.getReleaseDate();
+        List<String> characterUrl = movieData.getCharacterListUrlWrapper();
+
+        ArrayList<CharacterInfo> characterInfo = new ArrayList<>();
+        for (String url : characterUrl) {
+
+            CharacterInfo characterDetails = movieService.fetchCharacterData(url);
+            characterInfo.add(characterDetails);
+
+        }
+
+        modelMap.put("movieData", movieData);
+        modelMap1.put("characterInfo",characterInfo);
+
+        return "index";
+
 
     }
 
-    @ResponseBody
     @RequestMapping("/people")
-    public CharacterInfo displayCharacterInfo(){
+    public CharacterInfo displayCharacterInfo(ModelMap modelMap) {
         MovieData movieData = movieService.fetchMovieData();
         String characterUrl = movieData.getCharacterListUrlWrapper().get(0);
         CharacterInfo characterInfo = movieService.fetchCharacterData(characterUrl);
-        return characterInfo;
+        return "characterDetails";
 
     }
 }
